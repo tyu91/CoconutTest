57268ef11fc5956522427cfd1e9f400dc23c6a84
Tiffo Yu <tiffanyy9968@gmail.com>
2019-02-28 17:10:59 -0500

    created adapter to help populate recycler view with calendar data

--- app/src/main/java/com/coconuttest/tyu91/coconuttest/CalendarTestActivity.java
+++ app/src/main/java/com/coconuttest/tyu91/coconuttest/CalendarTestActivity.java
@@ -1,113 +1,85 @@
 package com.coconuttest.tyu91.coconuttest;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import me.tianshili.annotationlib.calendar.CalendarAnnotation;
+import me.tianshili.annotationlib.calendar.CalendarDataType;
+import me.tianshili.annotationlib.calendar.CalendarPurpose;
+import me.tianshili.annotationlib.commons.Visibility;
 
 public class CalendarTestActivity extends AppCompatActivity {
 
-    private int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    private int MY_PERMISSIONS_REQUEST_READ_CALENDAR = 1;
+    private int MY_PERMISSIONS_REQUEST_WRITE_CALENDAR = 2;
     private String[] projection;
     private String selection;
     private String androidID;
     private RecyclerView rvCalendar;
-
+    private CalendarTestAdapter calendarTestAdapter;
+    private ArrayList<String> calendarResults;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_test);
 
-//        recycleV = findViewById(R.id.rvRecycle);
-//        recycleV.setLayoutManager(new LinearLayoutManager(this));
-//        recycleV.setAdapter(adapterD);
+        calendarResults = new ArrayList<String>();
+        rvCalendar = findViewById(R.id.rvCalendar);
+        calendarTestAdapter = new CalendarTestAdapter(calendarResults);
+        rvCalendar.setAdapter(calendarTestAdapter);
+        rvCalendar.setLayoutManager(new LinearLayoutManager(this));
 
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            ActivityCompat.requestPermissions(this,
-//                    new String[]{Manifest.permission.READ_CONTACTS},
-//                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-//        }
-//
-//        String displayName = CalendarContract.Calendars.CALENDAR_DISPLAY_NAME;
-//
-//        projection = new String[]
-//                {CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
-//                        CalendarContract.Calendars.OWNER_ACCOUNT,
-//                        CalendarContract.Events.TITLE,
-//                        CalendarContract.Events.DESCRIPTION
-//                };
-//
-//        String[] projectionPrime = new String[]
-//                {CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
-//                        CalendarContract.Calendars.OWNER_ACCOUNT,
-//                        CalendarContract.Attendees.ATTENDEE_RELATIONSHIP,
-//                };
-//
-//        String[] projection1 = projection;
-//        String[] projection1Prime = projectionPrime;
-//
-////        projection1 = new String[]{CalendarContract.Calendars.CALENDAR_DISPLAY_NAME};
-//
-//        String[] projection2 = projection1;
-//        String[] projection2Prime = projection1Prime;
-//
-//        String[] projection3 = projection2;
-//        String[] projection3Prime = projection2Prime;
-//
+        calendarResults.add("dummy1");
+        calendarResults.add("dummy2");
+        calendarTestAdapter.notifyDataSetChanged();
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CALENDAR}, MY_PERMISSIONS_REQUEST_WRITE_CALENDAR);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CALENDAR},
+                    MY_PERMISSIONS_REQUEST_READ_CALENDAR);
+        }
+        projection = new String[] { CalendarContract.Events.CALENDAR_DISPLAY_NAME};
+
 //        //TODO: recursively resolve arguments to query string (do this in QueryUtil of privacyhelperplugin)
-//        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-//                + CalendarContract.Attendees.ATTENDEE_EMAIL + " = ?) AND ("
-//                + CalendarContract.Events.TITLE + " = ?) AND ("
-//                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-//        String selectionPrime = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-//                + CalendarContract.Attendees.ATTENDEE_EMAIL + " = ?) AND ("
-//                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-//
-//        String selection1 = selection;
-//        String selection1Prime = selectionPrime;
-//
-//        String selection2 = selection1;
-//        String selection2Prime = selection1Prime;
-//
-//        String selection3 = selection2;
-//        String selection3Prime = selection2Prime;
-//
-//        String[] selectionArgs = new String[]{"hera@example.com", "com.example",
-//                "hera@example.com"};
-//        @CalendarAnnotation(
-//                purpose = {CalendarPurpose.schedule},
-//                purposeDescription = {"Dummy variable to test"},
-//                dataType = {CalendarDataType.CALENDAR_DISPLAY_NAME, CalendarDataType.CALENDAR_OWNER, CalendarDataType.EVENT_TITLE, CalendarDataType.EVENT_DESCRIPTION},
-//                visibility = {Visibility.UNKNOWN})
-//        Cursor c;
-//
-//        c = this.getBaseContext().getContentResolver().query(CalendarContract.CONTENT_URI, projection3, selection3, null, null);
-//
-//        @CalendarAnnotation(
-//                purpose = {CalendarPurpose.schedule},
-//                purposeDescription = {"Dummy variable to test"},
-//                dataType = {CalendarDataType.CALENDAR_DISPLAY_NAME, CalendarDataType.CALENDAR_OWNER, CalendarDataType.ATTENDEE_RELATIONSHIP},
-//                visibility = {Visibility.UNKNOWN})
-//        Cursor d;
-//
-//        d = this.getBaseContext().getContentResolver().query(CalendarContract.CONTENT_URI, projection3Prime, selection3Prime, null, null);
-////
-//
-//
-//        projection = new String[]
-//                {CalendarContract.Events.ORGANIZER,
-//                        CalendarContract.Attendees.ATTENDEE_NAME
-//                };
-//
-//        projectionPrime = new String[]
-//                {CalendarContract.Events.ORGANIZER
-//                };
-//        String id = Settings.Secure.getString(getContentResolver(), androidID);
+        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + CalendarContract.Attendees.ATTENDEE_EMAIL + " = ?) AND ("
+                + CalendarContract.Events.TITLE + " = ?) AND ("
+                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+
+        String selectionPrime = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + CalendarContract.Attendees.ATTENDEE_EMAIL + " = ?) AND ("
+                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+
+        @CalendarAnnotation(
+                purpose = {CalendarPurpose.schedule},
+                purposeDescription = {"Dummy variable to test"},
+                dataType = {CalendarDataType.EVENT_TITLE},
+                visibility = {Visibility.UNKNOWN})
+        Cursor c;
+
+        c = this.getBaseContext().getContentResolver().query(CalendarContract.Events.CONTENT_URI, projection, null, null, null);
+        c.moveToFirst();
+        int numIter = c.getCount();
+
+        for(int i = 0; i < numIter; i++){
+            c.getInt(0);
+            c.moveToNext();
+        }
+
     }
 }
