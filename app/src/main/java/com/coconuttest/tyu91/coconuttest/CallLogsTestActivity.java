4a91e62c09881c7a78ed3e2d8924a7aca4c60d6a
Tiffo Yu <tiffanyy9968@gmail.com>
2019-05-22 23:56:32 -0700

    added comments to call log class

--- app/src/main/java/com/coconuttest/tyu91/coconuttest/CallLogsTestActivity.java
+++ app/src/main/java/com/coconuttest/tyu91/coconuttest/CallLogsTestActivity.java
@@ -28,12 +28,14 @@ public class CallLogsTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_logs_test);
 
+        //set up CallLogs recycler view and adapter
         callLogsResults = new ArrayList<String>();
         rvCallLogs = findViewById(R.id.rvCallLogs);
         callLogsTestAdapter = new CallLogsTestAdapter(callLogsResults);
         rvCallLogs.setAdapter(callLogsTestAdapter);
         rvCallLogs.setLayoutManager(new LinearLayoutManager(this));
 
+        //set up CallLogs permission request
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this,
@@ -41,6 +43,7 @@ public class CallLogsTestActivity extends AppCompatActivity {
                     MY_PERMISSIONS_REQUEST_READ_CALL_LOGS);
         }
 
+        //define query projection field
         String[] projectionCallLog = new String[]{
 //                CallLog.Calls.getLastOutgoingCall(this),
                 CallLog.Calls.CACHED_NAME,
@@ -57,8 +60,11 @@ public class CallLogsTestActivity extends AppCompatActivity {
                 dataType = {CallLogsDataType.CACHED_DATA, CallLogsDataType.NUMBER},
                 visibility = {Visibility.UNKNOWN})
         Cursor f;
+
+        //query call logs for results
         f = this.getBaseContext().getContentResolver().query(CallLog.Calls.CONTENT_URI, projectionCallLog, null, null, null);
 
+        //populate call logs recycler view with query results
         while (f.moveToNext()) {
             callLogsResults.add(f.getString(1));
         }
