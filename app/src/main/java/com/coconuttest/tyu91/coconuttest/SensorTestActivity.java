b5edf9331829f3699d51e7cb4ae18b2b7fd09b81
elijah-neundorfer <elijah.neundorfer@gmail.com>
2019-06-24 11:32:44 -0400

    Update to Sensor Test Activity

--- app/src/main/java/com/coconuttest/tyu91/coconuttest/SensorTestActivity.java
+++ app/src/main/java/com/coconuttest/tyu91/coconuttest/SensorTestActivity.java
@@ -3,23 +3,29 @@ package com.coconuttest.tyu91.coconuttest;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.HardwareBuffer;
 import android.hardware.Sensor;
+import android.hardware.SensorDirectChannel;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.TriggerEvent;
 import android.hardware.TriggerEventListener;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.MemoryFile;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.io.IOException;
 
 /**
  * Tests Coconut's ability to annotate APIs related to sensor data
  *
  * This activity simply activates all possible sensors and registers listeners.
- * 
+ *
  * It is not intended for actual use.
  *
  * @author Elijah Neundorfer 6/17/19
@@ -57,6 +63,12 @@ public class SensorTestActivity extends AppCompatActivity {
     private Sensor stationaryDetectSensor;
     private Sensor stepCounterSensor;
     private Sensor stepDetectorSensor;
+
+    //Objects required to test the SensorDirectChannel annotations
+    private SensorDirectChannel sensorDirectChannelMem;
+    private SensorDirectChannel sensorDirectChannelBuf;
+    private HardwareBuffer hardwareBuffer;
+    private MemoryFile memoryFile;
 
     //Handler necessary for some methods annotated by Coconut
     Handler handler = new Handler();
@@ -104,8 +116,31 @@ public class SensorTestActivity extends AppCompatActivity {
         stationaryDetectSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STATIONARY_DETECT);
         stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         stepDetectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-    }
 
+        //Initializing variables for DirectChannels
+        try {
+            memoryFile = new MemoryFile("Test",150);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            /**
+             * Have yet to figure out a way to successfully instantiate a HardwareBuffer and
+             * both SensorDirectChannels without significant coding time. Trying to find a good
+             * way to do this while being wary of the cost/benefit balance.
+             *
+             * Currently, the Annotations work fine, but this and other sections of code must
+             * be commented out for the activity to function.
+             */
+            hardwareBuffer = HardwareBuffer.create(
+                    1064960, 1, HardwareBuffer.BLOB, 1,
+                    HardwareBuffer.USAGE_CPU_READ_OFTEN | HardwareBuffer.USAGE_GPU_DATA_BUFFER
+                            | HardwareBuffer.USAGE_SENSOR_DIRECT_DATA);
+            sensorDirectChannelBuf = sensorManager.createDirectChannel(hardwareBuffer);
+
+            sensorDirectChannelMem = sensorManager.createDirectChannel(memoryFile);
+        }
+    }
 
     //Listens for changes in our triggered sensors and tracks data
     TriggerEventListener triggerEventListener = new TriggerEventListener() {
@@ -138,31 +173,35 @@ public class SensorTestActivity extends AppCompatActivity {
         if(accelerometerUncalibratedSensor != null) sensorManager.registerListener(sensorEventListener, accelerometerUncalibratedSensor, SensorManager.SENSOR_DELAY_NORMAL);
         if(ambientTemperatureSensor != null) sensorManager.registerListener(sensorEventListener, ambientTemperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
         if(devicePrivateBaseSensor != null) sensorManager.registerListener(sensorEventListener, devicePrivateBaseSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        if(gameRotationVectorSensor != null) sensorManager.registerListener(sensorEventListener, gameRotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        if(geomagneticRotationVectorSensor != null) sensorManager.registerListener(sensorEventListener, geomagneticRotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        if(gameRotationVectorSensor != null) sensorManager.registerListener(sensorEventListener,  gameRotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
+        if(geomagneticRotationVectorSensor != null) sensorManager.registerListener(sensorEventListener, geomagneticRotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL, 5);
         if(gravitySensor != null) sensorManager.registerListener(sensorEventListener, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL, 5);
         if(gyroscopeSensor != null) sensorManager.registerListener(sensorEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL,5);
         if(gyroscopeUncalibratedSensor != null) sensorManager.registerListener(sensorEventListener, gyroscopeUncalibratedSensor, SensorManager.SENSOR_DELAY_NORMAL, 5);
+
         //Ensuring we have permissions to use the heart rate sensor
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED) {
             if(heartBeatSensor != null) sensorManager.registerListener(sensorEventListener, heartBeatSensor, SensorManager.SENSOR_DELAY_NORMAL, 5);
-            if(heartRateSensor != null) sensorManager.registerListener(sensorEventListener, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL, 5);
+            if(heartRateSensor != null) sensorManager.registerListener(sensorEventListener, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL, handler);
         }
+
         if(lightSensor != null) sensorManager.registerListener(sensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL, handler);
         if(linearAccelerationSensor != null) sensorManager.registerListener(sensorEventListener, linearAccelerationSensor, SensorManager.SENSOR_DELAY_NORMAL, handler);
         if(lowLatencyOffBodyDetectionSensor != null) sensorManager.registerListener(sensorEventListener, lowLatencyOffBodyDetectionSensor, SensorManager.SENSOR_DELAY_NORMAL, handler);
         if(magneticFieldSensor != null) sensorManager.registerListener(sensorEventListener, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL, handler);
-        if(magneticFieldUncalibratedSensor != null) sensorManager.registerListener(sensorEventListener, magneticFieldUncalibratedSensor, SensorManager.SENSOR_DELAY_NORMAL, handler);
 
+        if(magneticFieldUncalibratedSensor != null) sensorManager.registerListener(sensorEventListener, magneticFieldUncalibratedSensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
         if(pose6DOFSensor != null) sensorManager.registerListener(sensorEventListener, pose6DOFSensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
         if(pressureSensor != null) sensorManager.registerListener(sensorEventListener, pressureSensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
         if(proximitySensor != null) sensorManager.registerListener(sensorEventListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
         if(relativeHumiditySensor != null) sensorManager.registerListener(sensorEventListener, relativeHumiditySensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
         if(rotationVectorSensor != null) sensorManager.registerListener(sensorEventListener, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
-        if(stepCounterSensor != null) sensorManager.registerListener(sensorEventListener, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
-        if(stepDetectorSensor != null) sensorManager.registerListener(sensorEventListener, stepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL, 5, handler);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if(stepCounterSensor != null && sensorDirectChannelMem != null) sensorDirectChannelMem.configure(stepCounterSensor, SensorDirectChannel.RATE_FAST);
+            if(stepDetectorSensor != null && sensorDirectChannelBuf != null) sensorDirectChannelBuf.configure(stepDetectorSensor, SensorDirectChannel.RATE_FAST);
+        }
 
         if(motionDetectSensor != null) sensorManager.requestTriggerSensor(triggerEventListener, motionDetectSensor);
         if(significantMotionSensor != null) sensorManager.requestTriggerSensor(triggerEventListener, significantMotionSensor);
@@ -172,8 +211,10 @@ public class SensorTestActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        //Clearing out our listeners
+        //Clearing out our listeners/DirectChannels
         sensorManager.unregisterListener(sensorEventListener);
+        sensorDirectChannelBuf.close();
+        sensorDirectChannelMem.close();
         sensorManager.cancelTriggerSensor(triggerEventListener, motionDetectSensor);
         sensorManager.cancelTriggerSensor(triggerEventListener, significantMotionSensor);
         sensorManager.cancelTriggerSensor(triggerEventListener, stationaryDetectSensor);
