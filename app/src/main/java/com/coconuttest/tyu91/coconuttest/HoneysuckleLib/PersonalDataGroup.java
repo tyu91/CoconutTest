71f0884682241cfbd0f398b8ea50ba0dc6433340
Tianshi Li <9413fi@gmail.com>
2019-09-08 11:39:20 -0400

    complete personal data group enum definition

--- app/src/main/java/com/coconuttest/tyu91/coconuttest/HoneysuckleLib/PersonalDataGroup.java
+++ app/src/main/java/com/coconuttest/tyu91/coconuttest/HoneysuckleLib/PersonalDataGroup.java
@@ -1,5 +1,16 @@
 package com.coconuttest.tyu91.coconuttest.HoneysuckleLib;
 
 public enum  PersonalDataGroup {
-    SMS
+    Location,
+    Unique_Identifier,
+    Contacts,
+    Calendar,
+    Camera,
+    Microphone,
+    CallLogs,
+    Personally_Identifiable_Information,
+    Sensor,
+    SMS,
+    User_File,
+    User_Input,
 }
