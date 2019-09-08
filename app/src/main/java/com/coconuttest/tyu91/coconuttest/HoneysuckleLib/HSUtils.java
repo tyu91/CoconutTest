46304d33af0a8370a299f005dc691b596b05b6be
Tianshi Li <9413fi@gmail.com>
2019-09-08 11:36:52 -0400

    permission notice dialog

--- app/src/main/java/com/coconuttest/tyu91/coconuttest/HoneysuckleLib/HSUtils.java
+++ app/src/main/java/com/coconuttest/tyu91/coconuttest/HoneysuckleLib/HSUtils.java
@@ -1,25 +1,36 @@
 package com.coconuttest.tyu91.coconuttest.HoneysuckleLib;
 
+
+import androidx.annotation.NonNull;
+
 public class HSUtils {
-    static String generatePurposesString(String [] purposes) {
+    static String generatePurposesString(@NonNull String [] purposes, String separator) {
         if (purposes == null || purposes.length == 0) {
             return "Not specified by the developer";
         }
         StringBuilder stringBuilder = new StringBuilder();
-        for (String purpose : purposes) {
-            stringBuilder.append("- ").append(purpose).append("<br>");
+        for (int i = 0 ; i < purposes.length ; i++) {
+            String purpose = purposes[i];
+            if (purpose == null || purpose.length() == 0) {
+                stringBuilder.append("- ").append("Not specified by the developer");
+            } else {
+                stringBuilder.append("- ").append(purpose);
+            }
+            if (i < purposes.length - 1) {
+                stringBuilder.append(separator);
+            }
         }
         return stringBuilder.toString();
     }
 
-    public static String generateEgressInfoString(String [] destinations) {
-        if (destinations == null || destinations.length == 0) {
+    public static String generateEgressInfoString(@NonNull String [] items) {
+        if (items.length == 0) {
             return null;
         }
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(destinations[0]);
-        for (int i = 1 ; i < destinations.length ; ++i) {
-            stringBuilder.append(", ").append(destinations[0]);
+        stringBuilder.append(items[0]);
+        for (int i = 1 ; i < items.length ; ++i) {
+            stringBuilder.append(", ").append(items[0]);
         }
         return stringBuilder.toString();
     }
--- app/src/main/java/com/coconuttest/tyu91/coconuttest/HoneysuckleLib/PermissionNotice.java
+++ app/src/main/java/com/coconuttest/tyu91/coconuttest/HoneysuckleLib/PermissionNotice.java
@@ -4,25 +4,71 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Build;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.PopupWindow;
 
-import com.coconuttest.tyu91.coconuttest.HoneysuckleGenerated.MyApplication;
-import com.coconuttest.tyu91.coconuttest.R;
+import androidx.annotation.RequiresApi;
 
-import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+import com.coconuttest.tyu91.coconuttest.HoneysuckleGenerated.AnnotationInfoMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.text.Html.FROM_HTML_MODE_LEGACY;
 
 public class PermissionNotice {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static void showDialog(Context context, PersonalDataGroup personalDataGroup, DialogInterface.OnCancelListener onCancelListener) {
         if (context == null) {
             return;
         }
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context).setTitle("Title").setMessage("Message");
+        AnnotationInfo [] annotationInfoList =
+                AnnotationInfoMap.getAnnotationInfoListByDataGroup(personalDataGroup);
+        ArrayList<String> purposeList = new ArrayList<>();
+        ArrayList<String> destinationList = new ArrayList<>();
+        ArrayList<String> dataTypeList = new ArrayList<>();
+        boolean dataLeaked = false;
+        for (AnnotationInfo annotationInfo : annotationInfoList) {
+            if (annotationInfo.purposes != null) {
+                purposeList.addAll(Arrays.asList(annotationInfo.purposes));
+            }
+            if (annotationInfo.leakedDataTypes != null) {
+                dataTypeList.addAll(Arrays.asList(annotationInfo.leakedDataTypes));
+            }
+            if (annotationInfo.destinations != null) {
+                destinationList.addAll(Arrays.asList(annotationInfo.destinations));
+            }
+            if (annotationInfo.dataLeaked) {
+                dataLeaked = true;
+            }
+        }
+        String purposesString = HSUtils.generatePurposesString(
+                purposeList.toArray(new String[0]), "<br>"); // "\n"
+        String destinationsString = HSUtils.generateEgressInfoString(destinationList.toArray(new String[0]));
+        String dataTypesString = HSUtils.generateEgressInfoString(dataTypeList.toArray(new String[0]));
+
+        Spanned egressDescriptionText;
+        if (dataLeaked) {
+            if (destinationsString == null && dataTypesString == null) {
+                egressDescriptionText = Html.fromHtml(String.format("<p>This app may send the %s data or information derived from the %s data out of the phone.</p><b>Purpose from the developer:</b><br>%s", personalDataGroup, personalDataGroup, purposesString), FROM_HTML_MODE_LEGACY);
+            } else if (destinationsString == null && dataTypesString != null) {
+                egressDescriptionText = Html.fromHtml(String.format("<p>This app will send %s out of the phone</p><b>Purpose from the developer:</b><br>%s", dataTypesString, destinationsString, purposesString), FROM_HTML_MODE_LEGACY);
+            } else if (destinationsString != null && dataTypesString == null) {
+                egressDescriptionText = Html.fromHtml(String.format("<p>This app will send the %s data or information derived from the %s data to %s</p><b>Purpose from the developer:</b><br>%s", personalDataGroup, personalDataGroup, destinationsString, purposesString), FROM_HTML_MODE_LEGACY);
+            } else {
+                egressDescriptionText = Html.fromHtml(String.format("<p>This app will send %s to %s</p><b>Purpose from the developer:</b><br>%s", dataTypesString, destinationsString, purposesString), FROM_HTML_MODE_LEGACY);
+            }
+        } else {
+            egressDescriptionText = Html.fromHtml(String.format("<p>This app will not send the %s data or information derived from the %s data out of the phone.</p><b>Purpose from the developer:</b><br>%s", personalDataGroup, personalDataGroup, purposesString), FROM_HTML_MODE_LEGACY);
+        }
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context)
+                .setTitle(Html.fromHtml(String.format("Privacy facts about <b>%s</b> access in this app", personalDataGroup.toString())))
+                .setMessage(egressDescriptionText);
         final AlertDialog dialog = dialogBuilder.create();
         dialog.setOnCancelListener(onCancelListener);
         final Window dialogWindow = dialog.getWindow();
